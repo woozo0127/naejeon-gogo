9c9223f78410b2697e5324c277b038552ad23e53
Woojo Kim <kwoojo0127@gmail.com>
2026-03-12 23:47:31 +0900

    refactor: layouts를 components/로 이동

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
-import * as styles from '#/client/layouts/app-layout.css';
-import { Sidebar } from '#/client/layouts/sidebar';
+import * as styles from '#/client/components/app-layout.css';
+import { Sidebar } from '#/client/components/sidebar';
 
 export const Route = createFileRoute('/_app')({
   component: AppLayout,
