bfd977a9e6ab28928bfad4ee6126e93e0b46e44a
Woojo Kim <kwoojo0127@gmail.com>
2026-03-12 23:50:41 +0900

    refactor: index.ts 배럴 export를 상대경로로 변경

---
+++ src/client/modules/match/index.ts
@@ -0,0 +1,10 @@
+export * from './model';
+export { useCancelMatch } from './use-cancel-match';
+export { useCompleteMatch } from './use-complete-match';
+export { useCreateMatch } from './use-create-match';
+export { useGenerateMatch } from './use-generate-match';
+export {
+  MATCHES_QUERY_KEY,
+  matchesQueryOptions,
+  useMatches,
+} from './use-matches';
---
+++ src/client/modules/member/index.ts
@@ -0,0 +1,10 @@
+export { filterMembersByName } from './filter-members-by-name';
+export * from './model';
+export { useCreateMember } from './use-create-member';
+export { useDeleteMember } from './use-delete-member';
+export {
+  MEMBERS_QUERY_KEY,
+  membersQueryOptions,
+  useMembers,
+} from './use-members';
+export { useUpdateMember } from './use-update-member';
